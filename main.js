3abaeb22920f555400748edd87cc60789f4a372f
matheuscal <matheuscalderon@hotmail.com>
2021-11-23 12:41:52 -0500

    add react-router

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes } from "react-router-dom";
 
 import App from './src/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+<BrowserRouter>
+<Routes>
+    <Route path="/" element={<App />}/>
+</Routes>
+</BrowserRouter>
+, document.getElementById('app'));
